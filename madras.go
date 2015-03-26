3f93fe251b379e247c9cf507b1aae87d60992a44
Abhimanyu M A <abhimanyu@hackerrank.com>
2015-03-27 03:20:36 +0530

    Add a initial dummy handler for simple requests

--- madras.go
+++ madras.go
@@ -2,28 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"net/http"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) *Page {
-	filename := title + ".txt"
-	body, _ := ioutil.ReadFile(filename)
-	return &Page{Title: title, Body: body}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2 := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
