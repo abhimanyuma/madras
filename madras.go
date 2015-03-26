5926e24a4ef3efc7f8aaef9cc5259e8055ec3506
Abhimanyu M A <abhimanyu@hackerrank.com>
2015-03-27 03:46:30 +0530

    Add a barely function in memory datastore
    This add a simple map variable based datastore that
    supports registering once and retrieval. This has
    lot of scope for errors no proper check done.

--- madras.go
+++ madras.go
@@ -3,13 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+var data_store map[string]string
+
+func registrationHandler(w http.ResponseWriter, r *http.Request) {
+	paths := strings.Split(string(r.URL.Path[1:]), "/")
+	key := paths[1]
+	value := paths[2]
+	val, ok := data_store[key]
+	if ok {
+		fmt.Fprintf(w, "Key - %s already present and set to %s", key, val)
+	} else {
+		data_store[key] = value
+		fmt.Fprintf(w, "Key - %s not present and now set to %s", key, value)
+	}
+}
+func retrivalHandler(w http.ResponseWriter, r *http.Request) {
+	key := string(r.URL.Path[1:])
+	val, ok := data_store[key]
+	if ok {
+		fmt.Fprint(w, val)
+	} else {
+		fmt.Fprintf(w, "Key %s - NOT PRESENT", key)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	data_store = make(map[string]string)
+	http.HandleFunc("/register/", registrationHandler)
+	http.HandleFunc("/", retrivalHandler)
 	http.ListenAndServe(":8080", nil)
 }
