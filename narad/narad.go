724348add0fe5a5410ae2be481c895a0f8f04b2b
Abhimanyu M A <abhimanyu@hackerrank.com>
2015-04-01 04:10:35 +0530

    Change database configuration to a config file

--- narad/narad.go
+++ narad/narad.go
@@ -2,7 +2,10 @@ package narad
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	_ "github.com/lib/pq"
+	"io/ioutil"
 )
 
 type NaradDB struct {
@@ -10,8 +13,36 @@ type NaradDB struct {
 	data_store map[string]string
 }
 
+type db_config struct {
+	User_name string
+	Password  string
+	Database  string
+}
+
+type config struct {
+	Db db_config
+}
+
+func getDBCredentials() config {
+	content, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		fmt.Print("Error:", err)
+	}
+
+	var madras_config config
+	err = json.Unmarshal(content, &madras_config)
+	if err != nil {
+		fmt.Print("Error:", err)
+	}
+	return madras_config
+}
+
 func Connect() (*NaradDB, error) {
-	db, err := sql.Open("postgres", "user=madras dbname=madras password=Js6K9j9JajtVfkl6dsKoLia9Lf68dk805bKf")
+	conf := getDBCredentials()
+	params := fmt.Sprintf("user=%s dbname=%s  password=%s",
+		conf.Db.User_name, conf.Db.Database, conf.Db.Password)
+
+	db, err := sql.Open("postgres", params)
 	db_struct := new(NaradDB)
 	db_struct.db_ref = db
 	return db_struct, err
